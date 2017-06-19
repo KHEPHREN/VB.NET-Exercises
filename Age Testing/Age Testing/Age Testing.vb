863e685440193a5d719e491ee7fcf9ca9ee1e08b
KHEPHREN <ingenieurkanga@yahoo.com>
2017-06-19 18:19:25 +0000

    Update Age Testing.vb
    This is what I think you could do to avoid as many errors as possible

--- Age Testing/Age Testing/Age Testing.vb
+++ Age Testing/Age Testing/Age Testing.vb
@@ -1,21 +1,25 @@
 ﻿Public Class Form1
 
     Private Sub Bn1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Bn1.Click
-        Dim age As String
-        Dim msg As String
+        Dim age As integer
+        Dim msg As string
+        try
+            age = cint(Tb1.Text)
+            msg = Tb2.Text
 
-        age = Tb1.Text
-        msg = Tb2.Text
-
-        If age <= 13 Then
-            Tb2.Text = "you are a child"
-        ElseIf age <= 21 Then
-            Tb2.Text = "you are a teenager"
-        ElseIf age <= 65 Then
-            Tb2.Text = "you are an adult"
-        ElseIf age > 65 Then
-            Tb2.Text = "you are old"
-        End If
-
+            If age <= 13 and age > 0 Then
+                Tb2.Text = "you are a child"
+            ElseIf age <= 21 and age > 13 Then
+                Tb2.Text = "you are a teenager"
+            ElseIf age <= 65 and age > 21 Then
+                Tb2.Text = "you are an adult"
+            ElseIf age > 65 and age <=200 Then
+                Tb2.Text = "you are old"
+            Else
+                Tb2.Text = "you can't exist!"
+            End If
+        catch ex as exeption
+            msgbox("Error: " & ex.message,vbcritical,"Error message")
+        End Try
     End Sub
 End Class
